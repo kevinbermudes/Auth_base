885a957e3f768c6e42d8bc85384287a94c207fb6
Kevin <kevin.bermudez@originpaht.com>
2025-04-24 23:48:23 +0200

    refactor(ProyectoAuthApplication): se borra comentario

--- src/main/java/org/example/proyectoauth/ProyectoAuthApplication.java
+++ src/main/java/org/example/proyectoauth/ProyectoAuthApplication.java
@@ -9,9 +9,6 @@ public class ProyectoAuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectoAuthApplication.class, args);
-        System.out.println("Password: admin123");
-        System.out.println(new BCryptPasswordEncoder().encode("admin123"));
-
     }
 
 }
